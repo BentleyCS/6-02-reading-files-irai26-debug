f1b8992484841a56af5204556ef152a01961e113
irai26-debug <irai26@bentleyschool.org>
2026-03-06 10:36:42 -0800

    Add files via upload

--- test_CSP_6_02_reading_files.py
+++ test_CSP_6_02_reading_files.py
@@ -1,8 +1,7 @@
 import CSP_6_02_reading_files as HW
 def test_to_string():
-     assert True
+    assert not(HW.toString("ExampleText.txt")=="Here is the text\ni am another line")
 def test_longest_line():
-     assert True
+    assert HW.longestLine("ExampleText.txt") == "Name\n"
 def test_to_Binary():
-
     assert True
